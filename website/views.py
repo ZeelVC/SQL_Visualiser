c705c21ee69f267949513d0a660488f4289f5c52
ZeelVC <zeelvc7@gmail.com>
2026-04-12 20:00:46 +0530

    Updated UI and code as per latest version of libraries.

--- website/views.py
+++ website/views.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, url_for
 
 views = Blueprint('views', __name__)
 
+
 @views.route('/')
 def home():
-    return render_template("base.html")
+    return redirect(url_for('auth.SQLViz'))
 
